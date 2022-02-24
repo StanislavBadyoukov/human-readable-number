488038834e910ae9b4cc5e1e0081efebe86ff80f
StanislavBadyoukov <stanislav.badyoukov@mail.ru>
2022-02-24 23:42:56 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function toReadable (number) {
-  
-}
+    if (number === 0) {return 'zero'}
+    const arr = ['zero', 'one', 'two', 'three', 'four', 'five',
+        'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+        'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    
+    const arrOfDozens  = ['zero', 'zero',
+        'twenty', 'thirty', 'forty', 'fifty', 'sixty',
+        'seventy', 'eighty', 'ninety']
+    
+    let result = '';
+    
+    const numberOfHundreds = Math.floor(number/100);
+    if (numberOfHundreds !== 0) {
+        result = `${arr[numberOfHundreds]} hundred`;
+    }
+    
+    const newNumber = number%100;
+    
+    if (newNumber === 0)  {return result}
+    
+    if (newNumber < 20) {
+        result = `${result} ${arr[newNumber]}`;
+        }
+         else {
+            numberOfDozens = Math.floor(newNumber/10);
+            result = `${result} ${arrOfDozens[numberOfDozens]}`
+    
+            const numberOfUnits = newNumber%10;
+    
+            if (numberOfUnits !== 0) {
+            result = `${result} ${arr[numberOfUnits]}`;
+            }
+         }
+    
+    return result.trim()
+    }
